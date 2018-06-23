748d72d47c1b0d9c96a5ed4df977b8374b8a5c62
Danil Vakhrushev <da.vakhr@gmail.com>
2018-06-23 20:55:42 +0400

    Setting init

---
+++ Views/Profile/settings.php
@@ -0,0 +1,26 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Setting | Soc Connections</title>
+    <link rel="stylesheet" href="/Assets/css/main.css"/>
+</head>
+<body>
+<header>
+    <div class="header">
+        <nav>
+            <ul class="header__list">
+                <li class="header__item logo-title"><a class="header__link" href="/">Social&nbsp;Connections</a></li>
+                <li class="header__item header__nav"><a class="header__link" href="./settings.html" id="login_button"><?php echo $_SESSION['username']; ?></a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+COMING SOOON
+<script src="/Assets/js/libs.min.js"></script>
+<script src="/Assets/js/main.js" charset="utf-8"></script>
+</body>
+</html>
